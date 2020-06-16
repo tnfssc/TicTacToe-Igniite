b1cfa7ad61bc4dbe1068981c3b76b2eedf4dff70
tnfssc <tnfssc@gmail.com>
2020-06-16 06:29:44 +0530

    Code-Push configured

--- android/app/src/main/java/com/tictactoe/MainApplication.java
+++ android/app/src/main/java/com/tictactoe/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,10 +28,15 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
           return packages;
         }
-
+        
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+        
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
       };
 
